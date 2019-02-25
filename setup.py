8af98276739a99c6f535e50f327ab8e2b2b1fc7f
Greg Fitch <greg.fitch@rice.edu>
2019-02-25 17:12:37 -0600

    bring in a req file

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ import os
 import codecs
 from setuptools import setup
 
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+reqs = [str(ir.req) for ir in install_reqs]
+
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
@@ -24,7 +33,7 @@ setup(
     long_description=read('README.rst'),
     py_modules=['pytest_securestore'],
     python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=['pytest>=3.7.0', 'pyyaml>=3.13', 'pyAesCrypt>=0.4.2'],
+    install_requires=reqs,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Pytest',
